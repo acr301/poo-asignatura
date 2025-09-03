4445337d919a618c7abae6ea1b72e65a613ce365
acr301 <acrothschuh@uamv.edu.ni>
2025-09-02 18:05:18 -0600

    ej encapsulacion + agregar jdtls al .gitignore

---
+++ intellij-poo/sesion-No3-02-09-2025/src/main/java/com/github/acr301/sesion3/Main.java
@@ -0,0 +1,9 @@
+package com.github.acr301.sesion3;
+import com.github.acr301.sesion3.models.Student;
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("s u per califra GILISTICO espi a li doso ");
+        System.out.println(student.getName());
+    }
+}
