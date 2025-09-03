d6a099ed5330410190b1bf3f5f080a1d83772c3d
acr301 <acrothschuh@uamv.edu.ni>
2025-09-03 08:59:18 -0600

    semana 2, encapsulacion

--- intellij-poo/sesion-No3-02-09-2025/src/main/java/com/github/acr301/sesion3/Main.java
+++ intellij-poo/sesion-No3-02-09-2025/src/main/java/com/github/acr301/sesion3/Main.java
@@ -3,7 +3,14 @@ import com.github.acr301.sesion3.models.Student;
 public class Main {
     public static void main(String[] args) {
         Student student = new Student();
-        student.setName("s u per califra GILISTICO espi a li doso ");
+        student.setNameLowerCaseNoSpaces("s u per califra GILISTICO espi a li doso ");
         System.out.println(student.getName());
+
+        student.setId("123");
+        System.out.println(student.getId());
+
+        Student student2 = new Student();
+        student2.setId("12345678");
+        System.out.println(student2.getId());
     }
 }
--- intellij-poo/sesion-No3-02-09-2025/src/main/java/com/github/acr301/sesion3/models/Student.java
+++ intellij-poo/sesion-No3-02-09-2025/src/main/java/com/github/acr301/sesion3/models/Student.java
@@ -2,12 +2,38 @@ package com.github.acr301.sesion3.models;
 
 public class Student {
   private String name;
+  private String career;
+  private String id;
 
   public String getName() {
     return name;
   }
-  public void setName(String name) {
+  public void setNameLowerCaseNoSpaces(String name) {
     this.name = name.toLowerCase().replaceAll("\\s", "");
   }
+
+    public String getCareer() {
+        return career;
+    }
+
+    public void setCareer(String career) {
+        this.career = career;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+      this.id = validateId(id);
+    }
+    public String validateId(String id) {
+        if (id.length() != 8) {
+            System.out.println("tiene que tener 8 dígitos!");
+            return "";
+        }
+        return id;
+    }
+
 }
 
